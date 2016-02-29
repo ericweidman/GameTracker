71efd7511280c65038a12634e07a620518ffdc0f
ericweidman <ericweidman@gmail.com>
2016-02-29 12:29:28 -0500

    Added h2 library.

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -75,6 +75,7 @@ public class Main {
                     return "";
                 })
         );
+
     }
 
     static User getUserFromSession(Session session){
@@ -82,3 +83,26 @@ public class Main {
         return users.get(name);
     }
 }
+
+
+//        Create the Connection and execute a query to create a games
+//        table that stores the game name and other attributes.
+
+//        Write a static method insertGame and run it in the /create-game route.
+//        It should insert a new row with the user-supplied information.
+
+//        Write a static method deleteGame and run it in the
+//        /delete-game route. It should remove the correct row using id.
+
+//        Write a static method selectGames that returns
+//        an ArrayList<Game> containing all the games in the database.
+
+//        Remove the global ArrayList<Game> and instead just call selectGames inside the "/" route.
+
+//        Add a form to edit the game name and other attributes,
+//        and create an /edit-game route. Write a static method updateGame
+//        and use it in that route. Then redirect to "/".
+
+
+//        Optional: Add a search form which filters the game list to only
+//        those games whose name contains the (case-insensitive) search string.
